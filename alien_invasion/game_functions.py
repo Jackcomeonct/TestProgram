953b9c20b38908310c2fa210fbf819a0ed347be0
Jackzhang <jackcomeon0923@outlook.com>
2018-03-19 23:31:41 +0800

    检查是否有外星人到达屏幕底端20180319

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -119,6 +119,15 @@ def ship_hit(ai_settings, stats, screen, ship, aliens,bullets):
     #暂停
     sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """检查是否有外星人到达屏幕底端"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #像飞船被撞到一样进行处理
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     '''检查是否有外星人位于屏幕边缘，并更新整群外星人的位置'''
     check_fleet_edges(ai_settings, aliens)
@@ -127,3 +136,5 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+    #检查是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
