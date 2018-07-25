eae5a865ae9d36fe64155fe8c9e29cca24ef671f
Jackzhang <jackcomeon0923@outlook.com>
2018-07-26 02:55:19 +0800

    击中外星人后更新得分20180726

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -66,18 +66,23 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_bu
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     bullets.update()
 
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     #检查是否有子弹击中了外星人，如果是这样就删除相应的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if collisions:
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         #删除所有的子弹,加快游戏节奏，并创建新的外星人
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -18,6 +18,8 @@ class Settings():
 
         #以什么样的速度加快游戏节奏
         self.speedup_scale = 1.1
+        #外星人点数提高的倍数
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -29,9 +31,12 @@ class Settings():
 
         #fleet_direction为1代表向右移动，为-1代表向左移动
         self.fleet_direction = 1
+        #计分
+        self.alien_points = 50
 
     def increase_speed(self):
-        """提高速度设置"""
+        """提高速度设置和外星人点数"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
